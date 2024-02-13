50fbe50f15a10410f9b6cabbf6e73c1932043cff
damian127721 <106865168+damian127721@users.noreply.github.com>
2024-02-13 20:27:34 +0100

    redirect

--- backend/middlewares/errorMiddlewares.js
+++ backend/middlewares/errorMiddlewares.js
@@ -1,16 +1,17 @@
 const errorCustom = (err, req, res, next) => {
-    if (res.statusCode === 200) {
-        res.status(500)
-    }
+  if (res.statusCode === 200) {
+    res.status(500);
+  }
 
-    res.json({
-        message: err.message
-    })
-}
+  res.json({
+    message: err.message,
+  });
+};
 
 const notFound = (req, res, next) => {
-    res.status(404)
-    next(new Error("Not found"))
-}
+  res.status(404);
+  res.redirect("https://project-x-vzmk.onrender.com");
+  next(new Error("Not found"));
+};
 
-module.exports = {errorCustom, notFound}
+module.exports = { errorCustom, notFound };
